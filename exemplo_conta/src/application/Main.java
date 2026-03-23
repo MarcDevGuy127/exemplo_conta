e34d0c601a4894dd7457ed1f3df04eddd968a74e
MarcDevGuy127 <marceloh.github@gmail.com>
2026-03-23 13:37:40 -0300

    feat(Main): turn Conta private to public attributes

--- exemplo_conta/src/application/Main.java
+++ exemplo_conta/src/application/Main.java
@@ -1,10 +1,17 @@
 package application;
 
+import entities.Conta;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Conta c1 = new Conta();
+		
+		c1.titular = "Marcelo";
+		c1.saldo = 200;
+		
+		
 	}
 
 }
